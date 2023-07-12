3418050a7e67b7d3931f198b6803da5443bd9686
Ardhiansyah09 <ardhiansyah991@gmail.com>
2023-07-12 15:47:50 +0700

    ganti login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pengarsipan Pertanahan DPTR</title>
+    <title>Web Administrasi Balai PPA DP3AP2 DIY</title>
     <link rel="stylesheet" href="/css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -12,11 +12,11 @@
 <body>
   
   <!-- Navbar Login -->
-  <nav class="navbar navbar-expand-lg" style="background-color: #A0522D">
+  <nav class="navbar navbar-expand-lg" style="background-color: #4e73df">
     <div class="container-fluid">
         <a class="navbar-brand d-flex" href="#">
             <img src="{{ asset('assets/img/logo.jpg') }}" alt="" width="50" height="60" class="d-inline-block align-text-top">
-            <h3>PENGARSIPAN BIDANG P5 DISPERTARU DIY</h3>
+            <h3>WEB ADMINISTRASI BALAI PPA DIY</h3>
           </a>
     </div>
   </nav>
