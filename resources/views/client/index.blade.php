d01e2ec6ff0ae231f935e009d2efc010a23d825a
Ardhiansyah09 <ardhiansyah991@gmail.com>
2023-08-04 15:36:44 +0700

    puspaga logo

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -185,7 +185,7 @@
                     <a href="/layanan/puspaga" class="text-decoration-none text-reset" target="_blank">
                         <img src="{{ asset('assets/img/logo2.png') }}" class="img-fluid" alt="">
                         <div class="text-center mt-4">
-                            <div class="layanan-title">PUSPAGA</div>
+                            <div class="layanan-title">PUSPAGA PRIMA DIY</div>
                             <div class="layanan-desciption">“Puspaga merupakan layanan konseling psikologi
                                 terkait pengasuhan, tumbuh kembang anak, remaja,
                                 serta permasalahan perempuan dan keluarga."</div>
